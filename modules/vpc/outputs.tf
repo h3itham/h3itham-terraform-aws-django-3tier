8b0d5941f5efedc0b0d9a44d0acb0f439c1d779d
Haitham Elabd <haithamelabd@outlook.com>
2024-04-08 01:00:54 +0200

    nat id

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,12 +2,10 @@
 output "vpc_id" {
     value = aws_vpc.vpc.id
 }
-
 # OUTPUT TO RETRIVE INTERNET GATEWAY ID
 output "igw_id" {
     value = aws_internet_gateway.igw.id
 }
-
 #OUTPUT TO RETRIVE NAT ID
 output "nat_id" {
     value = aws_nat_gateway.nat.id
